c44037f570b05849cad89dcae9742e20966585cc
sh3ll <63223659+evilsh3ll@users.noreply.github.com>
2023-08-07 09:46:10 +0200

    Create censys-quota-refilll.sh

---
+++ censys-quota-refilll.sh
@@ -0,0 +1,34 @@
+# PUT HERE YOUR FREE API CREDENTIALS
+censys_api_list=(
+	"API_ID:API_SECRET", # 250 calls each key
+	"API_ID:API_SECRET",
+	"API_ID:API_SECRET",
+	"API_ID:API_SECRET",
+)
+# REFILL CALLBACK
+function check_censys_and_refill()
+  curr_quota=$(censys account | grep "/" | awk '{print $5}')
+
+  if [ "$curr_quota" -gt 249 ]; then
+    echo "Censys limit quota reached: $curr_quota"
+    for curr_api in "${censys_api_list[@]}"
+    do	
+        # GET AND REFILL NEW KEYS
+    	  api_id=$(echo "$curr_api" | cut -d ':' -f 1)
+	      api_secret=$(echo "$curr_api" | cut -d ':' -f 2)
+        sed -i "s/api_id =.*/api_id = $api_id/" ~/.config/censys/censys.cfg
+        sed -i "s/api_secret =.*/api_secret = $api_secret/" ~/.config/censys/censys.cfg
+
+        # PRINT NEW QUOTA
+        check=$(censys account | grep "/" | awk '{print $5}')
+        if [ "$check" -lt 250 ]; then
+        	echo "Censys API switched - New Quota: $check"
+        	return # EXIT IF THERE IS QUOTA
+        fi
+    done
+    
+    
+  fi
+}
+
+check_censys_and_refill
