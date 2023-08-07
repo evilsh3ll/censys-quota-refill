9de4d11e0c58e4e25e706235cf64d54c96a1cd26
sh3ll <63223659+evilsh3ll@users.noreply.github.com>
2023-08-07 09:57:42 +0200

    Update censys-quota-refilll.sh

--- censys-quota-refilll.sh
+++ censys-quota-refilll.sh
@@ -5,7 +5,8 @@ censys_api_list=(
 	"API_ID:API_SECRET",
 	"API_ID:API_SECRET",
 )
-# REFILL CALLBACK
+
+# THE REFILL CALLBACK
 function check_censys_and_refill()
   curr_quota=$(censys account | grep "/" | awk '{print $5}')
 
@@ -15,13 +16,13 @@ function check_censys_and_refill()
     do	
         # GET AND REFILL NEW KEYS
     	  api_id=$(echo "$curr_api" | cut -d ':' -f 1)
-	      api_secret=$(echo "$curr_api" | cut -d ':' -f 2)
-        sed -i "s/api_id =.*/api_id = $api_id/" ~/.config/censys/censys.cfg
-        sed -i "s/api_secret =.*/api_secret = $api_secret/" ~/.config/censys/censys.cfg
+          api_secret=$(echo "$curr_api" | cut -d ':' -f 2)
+          sed -i "s/api_id =.*/api_id = $api_id/" ~/.config/censys/censys.cfg
+          sed -i "s/api_secret =.*/api_secret = $api_secret/" ~/.config/censys/censys.cfg
 
         # PRINT NEW QUOTA
-        check=$(censys account | grep "/" | awk '{print $5}')
-        if [ "$check" -lt 250 ]; then
+        new_quota=$(censys account | grep "/" | awk '{print $5}')
+        if [ "$new_quota" -lt 250 ]; then
         	echo "Censys API switched - New Quota: $check"
         	return # EXIT IF THERE IS QUOTA
         fi
