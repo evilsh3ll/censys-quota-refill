d5cf89f277f4135c7527576690f07d9bc7810dc0
sh3ll <63223659+evilsh3ll@users.noreply.github.com>
2023-08-11 16:05:27 +0200

    FEATURE: list all quota available
    added a function to list all quota available across all accounts

--- censys-quota-refilll.sh
+++ censys-quota-refilll.sh
@@ -6,7 +6,7 @@ censys_api_list=(
 	"API_ID:API_SECRET",
 )
 
-# THE REFILL CALLBACK
+# REFILL QUOTA FROM LIST
 function check_censys_and_refill()
   curr_quota=$(censys account | grep "/" | awk '{print $5}')
 
@@ -29,10 +29,26 @@ function check_censys_and_refill()
         	echo "Censys API switched - New Quota: $check"
         	return # EXIT IF THERE IS QUOTA
         fi
-    done
-    
-    
+    done    
   fi
 }
 
+# LIST AVAILABLE QUOTA FOR ALL ACCOUNTS
+function list_censys_api_quota() {
+  for curr_api in "${censys_api_list[@]}"
+  do
+      api_id=$(echo "$curr_api" | cut -d ':' -f 1)
+      api_secret=$(echo "$curr_api" | cut -d ':' -f 2)
+      sed -i "s/api_id =.*/api_id = $api_id/" ~/.config/censys/censys.cfg
+      sed -i "s/api_secret =.*/api_secret = $api_secret/" ~/.config/censys/censys.cfg
+      p1=$(censys account | grep "/" | awk '{print $5}')
+      p2=$(censys account | grep "/" | awk '{print $7}')
+      email=$(censys account | grep "Email" | awk '{print $4}')
+      echo "${email} reached ${p1}/${p2} quota"
+  done   
+}
+
+# LIST QUOTA
+list_censys_api_quota
+# REFILL ACCOUNT IF NEEDED
 check_censys_and_refill
